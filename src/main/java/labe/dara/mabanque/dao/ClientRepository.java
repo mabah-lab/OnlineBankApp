ac76d603c5f0df79b9e1b51777ae82766e79410d
b0016381 <b0016381@SIM086L.cpasbru.be>
2020-10-12 16:27:40 +0200

    second commit

--- src/main/java/labe/dara/mabanque/dao/ClientRepository.java
+++ src/main/java/labe/dara/mabanque/dao/ClientRepository.java
@@ -3,7 +3,7 @@ package labe.dara.mabanque.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import labe.dara.mabanque.entities.Client;
-
+//comment
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
